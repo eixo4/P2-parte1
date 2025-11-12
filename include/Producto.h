c7e5855899e1019abbd763f565199b6d426915fc
Edu <145373957+edoverso@users.noreply.github.com>
2025-11-12 10:34:38 -0500

    Clase Producto.h creada

---
+++ include/Producto.h
@@ -0,0 +1,7 @@
+//Definan una clase o estructura de datos para representar un Producto. Cada Producto debe tener al menos los siguientes atributos:
+//id (entero único, por ejemplo, generado secuencialmente)
+//nombre (cadena de texto)
+//precio (número flotante o decimal)
+//categoria (cadena de texto, por ejemplo: "Electrónica", "Ropa", "Libros", "Hogar")
+//stock (entero)
+//calificacionPromedio (número flotante, entre 1.0 y 5.0)
